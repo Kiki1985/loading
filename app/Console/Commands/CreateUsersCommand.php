317daec6088847b367eeecd8218d264e1e33e139
Kiki1985 <miroslav.dojchinovic@gmail.com>
2020-07-08 16:44:06 +0200

    php artisan command added - create:users

---
+++ app/Console/Commands/CreateUsersCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+use App\User;
+
+class CreateUsersCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'create:users';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new users';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    /*public function __construct()
+    {
+        parent::__construct();
+    }*/
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        /*$user= User::create([
+            'fName' => 'Miroslav',
+            'lName' => 'Dojcinovic'
+        ])*/
+
+        $this->info("Hello World");
+    }
+}
